430a8ec45cefc85b076eacd8a8cb94ce742976b3
Matthew Searle <65017209+matthewsearle01@users.noreply.github.com>
2024-02-02 15:20:36 +0000

    update API endpoint in list.ts

--- app/ui/case-studies/table.tsx
+++ app/ui/case-studies/table.tsx
@@ -27,103 +27,81 @@ export default async function CaseStudyTable({
       <div className="inline-block min-w-full align-middle">
         <div className="rounded-lg bg-gray-50 p-2 md:pt-0">
           <div className="md:hidden">
-            {data && data.name && (
-              <div key={data.name} className="mb-2 w-full rounded-md bg-white p-4">
+            {data.map((data, index) => (
+              <div key={index} className="mb-2 w-full rounded-md bg-white p-4">
                 <div className="flex items-center justify-between border-b pb-4">
                   <div>
                     <div className="mb-2 flex items-center">
-                      <p>{data.name}</p>
+                      <p>{data.Key}</p>
                     </div>
-                    <p className="text-sm text-gray-500">{data.climate}</p>
+                    <p className="text-sm text-gray-500">
+                      Last Modified: {data.LastModified}
+                    </p>
                   </div>
+                  <p>ETag: {data.ETag}</p>
                 </div>
                 <div className="flex w-full items-center justify-between pt-4">
                   <div>
-                    <p className="text-xl font-medium">{data.diameter}</p>
-                    <p>{data.gravity}</p>
+                    <p className="text-xl font-medium">
+                      Checksum Algorithm: {data.ChecksumAlgorithm.join(', ')}
+                    </p>
+                    <p>Size: {data.Size} bytes</p>
                   </div>
-                  <div className="flex justify-end gap-2">
-                    <p>{data.orbital_period}</p>
-                    <p>{data.population}</p>
-                    <p>{data.rotation_period}</p>
-                    <p>{data.surface_water}</p>
-                    <p>{data.terrain}</p>
-                  </div>
+                  <p>Storage Class: {data.StorageClass}</p>
                 </div>
               </div>
-            )}
+            ))}
           </div>
           <table className="hidden min-w-full text-gray-900 md:table">
             <thead className="rounded-lg text-left text-sm font-normal">
               <tr>
                 <th scope="col" className="px-4 py-5 font-medium sm:pl-6">
-                  Name
+                  Key
                 </th>
                 <th scope="col" className="px-3 py-5 font-medium">
-                  Climate
+                  Last Modified
                 </th>
                 <th scope="col" className="px-3 py-5 font-medium">
-                  Diameter
+                  ETag
                 </th>
                 <th scope="col" className="px-3 py-5 font-medium">
-                  Gravity
+                  Checksum Algorithm
                 </th>
                 <th scope="col" className="px-3 py-5 font-medium">
-                  Orbital Period
+                  Size
                 </th>
                 <th scope="col" className="px-3 py-5 font-medium">
-                  Population
-                </th>
-                <th scope="col" className="px-3 py-5 font-medium">
-                  Rotation Period
-                </th>
-                <th scope="col" className="px-3 py-5 font-medium">
-                  Surface Water
-                </th>
-                <th scope="col" className="px-3 py-5 font-medium">
-                  Terrain
+                  Storage Class
                 </th>
               </tr>
             </thead>
             <tbody className="bg-white">
-              {data && data.name && (
-                <tr>
+              {data.map((data, index) => (
+                <tr key={index}>
                   <td className="whitespace-nowrap py-3 pl-6 pr-3">
                     <div className="flex items-center gap-3">
-                      <p>{data.name}</p>
+                      <p>{data.Key}</p>
                     </div>
                   </td>
                   <td className="whitespace-nowrap px-3 py-3">
-                    {data.climate}
+                    {data.LastModified}
                   </td>
+                  <td className="whitespace-nowrap px-3 py-3">{data.ETag}</td>
                   <td className="whitespace-nowrap px-3 py-3">
-                    {data.diameter}
+                    {data.ChecksumAlgorithm.join(', ')}
                   </td>
+                  <td className="whitespace-nowrap px-3 py-3">{data.Size} bytes</td>
                   <td className="whitespace-nowrap px-3 py-3">
-                    {data.gravity}
-                  </td>
-                  <td className="whitespace-nowrap px-3 py-3">
-                    {data.orbital_period}
-                  </td>
-                  <td className="whitespace-nowrap px-3 py-3">
-                    {data.population}
-                  </td>
-                  <td className="whitespace-nowrap px-3 py-3">
-                    {data.rotation_period}
-                  </td>
-                  <td className="whitespace-nowrap px-3 py-3">
-                    {data.surface_water}
-                  </td>
-                  <td className="whitespace-nowrap px-3 py-3">
-                    {data.terrain}
+                    {data.StorageClass}
                   </td>
                 </tr>
-              )}
+              ))}
             </tbody>
           </table>
         </div>
       </div>
     </div>
   );
+  
   
 }
--- pages/api/list.ts
+++ pages/api/list.ts
@@ -3,7 +3,8 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const externalServiceUrl = process.env.EXTERNAL_SERVICE_URL;
 
-  const response = await fetch(`https://swapi.dev/api/planets/1/`);
+  // const response = await fetch(`https://swapi.dev/api/planets/1/`);
+  const response = await fetch(`${externalServiceUrl}/doc_store/list`);
 
   if (!response.ok) {
     res.status(500).json({ error: 'Failed to fetch document list' });
